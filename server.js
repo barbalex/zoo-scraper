a0cc5ab352722f39ed159bb0c9f7654dda1047ff
Alexander Gabriel <alex.barbalex@gmail.com>
2021-12-12 22:42:58 +0100

    correct

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ const run = async () => {
     from: 'Nachtwandel-Bot <alex@barbalex.ch>',
     //to: 'alex.barbalex@gmail.com',
     to: 'barbara.barbalex@gmail.com, alex.barbalex@gmail.com',
-    subject: `${myItems} mal Nachtwandeln im September`,
+    subject: `${myItems?.length ?? 0} mal Nachtwandeln im September`,
     html,
   }
   client.messages
